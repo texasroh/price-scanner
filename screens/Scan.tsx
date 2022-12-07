c60650cc365566ea15ebebeb91a1410c138748c9
texasroh <texasroh@gmail.com>
2022-12-07 02:10:41 -0500

    add methods for async storage

--- screens/Scan.tsx
+++ screens/Scan.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import styled from "styled-components/native";
 import { RootStackParamList } from "../navigator/RootStack";
+import { addHistory } from "../storage";
 
 const Container = styled.View`
   flex: 1;
@@ -27,6 +28,7 @@ const Scan: React.FC<NativeStackScreenProps<RootStackParamList, "Scan">> = ({
   }, []);
 
   const handleBarCodeScanned = ({ type, data }: BarCodeScannerResult) => {
+    addHistory(data);
     replace("Search", { barcode: data });
   };
 
--- storage.ts
+++ storage.ts
@@ -1,23 +1,35 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const SEARCH_LIST = "search_list";
+const SEARCH_HISTORY = "search_history";
 
 export const storeHistory = async (value: any) => {
   try {
-    await AsyncStorage.setItem(SEARCH_LIST, JSON.stringify(value));
+    await AsyncStorage.setItem(SEARCH_HISTORY, JSON.stringify(value));
   } catch (e) {
     console.log(e);
   }
 };
 
-export const addHistory = async (value: any) => {};
+export const addHistory = async (value: any) => {
+  try {
+    await AsyncStorage.mergeItem(SEARCH_HISTORY, JSON.stringify(value));
+    loadHistory();
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const loadHistory = async () => {
   try {
-    const value = await AsyncStorage.getItem(SEARCH_LIST);
+    const value = await AsyncStorage.getItem(SEARCH_HISTORY);
+    console.log(value);
     return value != null ? JSON.parse(value) : null;
   } catch (e) {
     console.log(e);
     return null;
   }
 };
+
+export const deleteHistory = async () => {};
+
+export const deleteAllHistory = async () => {};
