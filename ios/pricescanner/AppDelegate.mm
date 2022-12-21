afcc69f2f5ff2ac74ab2742398f50c7f51a6dffd
texasroh <texasroh@gmail.com>
2022-12-20 23:03:24 -0500

    install codePush for ios

--- ios/pricescanner/AppDelegate.mm
+++ ios/pricescanner/AppDelegate.mm
@@ -11,6 +11,7 @@
 #import <AppCenterReactNative.h>
 #import <AppCenterReactNativeAnalytics.h>
 #import <AppCenterReactNativeCrashes.h>
+#import <CodePush/CodePush.h>
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -98,7 +99,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
